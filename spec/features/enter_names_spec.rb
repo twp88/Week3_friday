ecdbea10dc996be1b643c0ab1aa2bb1451529075
cgulli <caitlin.gulliford@gmail.com>
2016-10-11 17:20:44 +0100

    Up to Step 18 of battle weekly challenge

---
+++ spec/features/enter_names_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+feature 'Enter names' do
+  scenario 'submitting names' do
+    visit('/')
+    fill_in :player_1_name, with: 'Kanye'
+    fill_in :player_2_name, with: 'Kittens'
+    click_button 'Submit'
+    expect(page).to have_content 'Kanye vs. Kittens'
+  end
+end
